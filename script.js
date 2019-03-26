c1eec1ec4ec881d28166e625a9343e9f8d1e9660
pavanreddy <mukkarasfdc@gmail.com>
2019-03-26 16:29:48 +0530

    updated

--- script.js
+++ script.js
@@ -20,18 +20,27 @@ let  sec;
 //validating the text entered in submitted text area, by using event listener with keyup event
 submittedtext.addEventListener('keyup',valTyping);
 let defTextArray = defaulttext.value;
-
+let wordCount=1;
+let length =0;
 function valTyping() {
-
     let subTextArray = submittedtext.value;
+    let spaceRepetition;
     stopTimerRecalling(subTextArray);
-    if (subTextArray.length >=0) {
+
+    if (subTextArray.length >0) {
+
         for (let i = 1; i <= subTextArray.length; i++) {
             if (defTextArray.substring(0,i) === subTextArray.substring(0,i)) {
                 submittedtext.style.background = 'green';
                 congratsText.textContent='';
-            }
-            else {
+
+                   if(subTextArray.substring(i)!==' ' && subTextArray.substring(0,i).length < defTextArray.length &&subTextArray.substring(i-1)===' ' && subTextArray.length>length+1 )
+                       {
+                           wordCount++;
+                           length=subTextArray.length;
+                         }
+                }
+                else {
                 submittedtext.style.background = 'red';
             }
         }
@@ -68,7 +77,8 @@ function resetFunction() {
     seconds.textContent='00';
     defaulttext.textContent='Select a Content';
     speed.textContent = '000';
-
+    totalSeconds=0;
+    totalSpeed=0;
 }
 // disable copy , cut , paste options in the textarea box
 
@@ -84,19 +94,22 @@ function stopTimerRecalling(arraylength) {
         calling++;
         sec= setInterval(timer,1000);
     }
-
 }
 
 
 let min=0;
 let count=0;
+let wordCountMin=0;
+let wordCountSec=0;
 function timer(){
     count++;
+    wordCountSec=count;
     seconds.textContent=count;
     if(count%60==0){
         count=0;
         min= min+1;
         minutes.textContent=min;
+        wordCountMin=min;
     }
 }
 
@@ -119,22 +132,23 @@ function resumeTimer() {
 
 }
 
-// evaluating the typing speed => make every task with 200
+// evaluating the typing speed => make every task with 100 words
+let totalSeconds;
+let totalSpeed;
 function findSpeed() {
-        var typingspeed= ((100/((min*60)+sec))*60);
-    speed.textContent = typingspeed;
+   totalSeconds = wordCountMin*60 + wordCountSec;
+   totalSpeed = (wordCount/totalSeconds)*60;
+   speed.textContent='totalSpeed';
+
 }
 
 //Assigning Contents
 cont1.addEventListener('click',content1);
 function content1() {
     resetFunction();
-    console.log('clicked');
     defaulttext.textContent = 'content 1 dolor sit amet, consectetur adipisicing elit. Aperiam cum eius eveniet fugiat id illo magni nemo nisi nulla officia perspiciatis praesentium quasi quibusdam, quo repellat sapiente sunt suscipit tempora totam velit. Ab, aliquam aperiam, consequuntur dolorum ea, eaque eligendi eum ipsam maiores nobis non omnis optio voluptatum! Asperiores assumenda blanditiis corporis cum cupiditate dicta distinctio doloremque dolores ducimus eveniet expedita, facilis harum illo illum ipsa magni maiores mollitia nostrum perferendis placeat quis quo repellat sapiente sint suscipit tempora tempore totam ullam vero voluptatem. Cumque dolor, ea eius ipsa labore nemo obcaecati sint tempora. Cum dolores labore optio pariatur voluptatibus?';
     defTextArray = defaulttext.value;
     valTyping();
-
-
 }
 cont2.addEventListener('click',content2);
 function content2() {
